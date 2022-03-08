277d2fde5e315da520a759b3dc9a80b689dbb424
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-03-07 18:48:11 -0800

    added AddTransaction Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
