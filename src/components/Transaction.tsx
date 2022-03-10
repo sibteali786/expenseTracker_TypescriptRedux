4e3b718558d6e7b8e7e535d741d6d3505432f1b7
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-03-09 23:44:58 -0800

    added plus and minus classes according to transaction amount

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -12,10 +12,13 @@ type props = {
 
 export const Transaction = (props: props) => {
   const transaction = props.transaction;
-
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li className="minus">
-      {transaction.text} <span>-$400</span>
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   );
--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -9,7 +9,7 @@ const TransactionList = () => {
       <h3>History</h3>
       <ul className="list">
         {transactions.map((transaction) => (
-          <Transaction transaction={transaction} />
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </Fragment>
