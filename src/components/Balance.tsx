12cca5eb753fddf5231d60e67ce71e2df5279abd
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2022-03-11 12:40:52 +0500

    added states in all components

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,10 +1,15 @@
-import React, { Fragment } from "react";
-
+import React, { Fragment, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 const Balance = () => {
-  return (
+  const {transactions} = useContext(GlobalContext);
+  const amounts = transactions.map(transaction=>transaction.amount);
+  const total = amounts.reduce((acc,item)=>(acc+=item),0).toFixed(2);
+  console.log(amounts);
+  
+  return ( 
     <Fragment>
       <h4>Your Balance</h4>
-      <h1 id="balance"> $0.00</h1>
+      <h1 id="balance"> ${total}</h1>
     </Fragment>
   );
 };
--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -16,9 +16,9 @@ type childProps = {
 const initialState = {
   transactions: [
     { id: 1, text: "Flower", amount: -20 },
-    { id: 1, text: "Salary", amount: 300 },
-    { id: 1, text: "Book", amount: -10 },
-    { id: 1, text: "Camera", amount: 150 }
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 }
   ]
 };
 
@@ -33,6 +33,9 @@ export const GlobalProvider = ({ children }: childProps) => {
     }
   }
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions (called using dispatch)
+  
 
   return (
     <GlobalContext.Provider value={{ transactions: state.transactions }}>
